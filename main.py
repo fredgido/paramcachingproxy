90106e80d4f6fc9416857aaca602b19955b310ab
Fred Fernandes <fred.fernandes@neonumy.com>
2022-12-24 16:35:41 +0000

    wip

--- main.py
+++ main.py
@@ -24,33 +24,59 @@ async def get_movies():
     Response
     return Response(200, content=Content(b"text/plain", a))
 
+
 import pathlib
 
 twitter_media_path = pathlib.Path("twitter_media")
 twitter_media_path.mkdir(exist_ok=True)
 
-image_name_url =1
+image_name_url_regex_str = r"\/media\/(?P<name>[a-zA-Z\d]*)\.(?P<extension>[a-z]*)"
+import re
+
+image_name_url_regex = re.compile(image_name_url_regex_str)
+
+test_url = "https://pbs.twimg.com/media/FkqZQYrakAA5P5h.jpg:orig"
+
+match = image_name_url_regex.search(test_url)
+print(match.groupdict())
+
 
 @app.route("/stream_test")
-async def get_movies():
-    from blacksheep.client import ClientSession
+async def stream_test():
 
     test_url = "https://pbs.twimg.com/media/FkqZQYrakAA5P5h.jpg:orig"
 
-    async def client_example(loop):
-        async with ClientSession() as client:
-            response = await client.get(test_url)
+    match = image_name_url_regex.search(test_url)
 
-            assert response is not None
-            text = await response.text()
-            print(text)
+    if not match:
+        print(test_url, "failed")
+        return Response(200, content=Content(b"text/plain", b"Not Found"))
 
     async def provider():
         async with client.stream("GET", test_url) as response:
-            async for chunk in response.aiter_bytes():
+            async for chunk in response.aiter_bytes(chunk_size=65534):
                 yield chunk
 
     return Response(200, content=StreamedContent(b"image/jpeg", provider))
+
+
+@app.route("/stream_test2")
+async def stream_test2():
+
+    test_url = "https://pbs.twimg.com/media/FkqZQYrakAA5P5h.jpg:orig"
+
+    match = image_name_url_regex.search(test_url)
+
+    if not match:
+        print(test_url, "failed")
+        return Response(200, content=Content(b"text/plain", b"Not Found"))
+
+    async with client.stream("GET", test_url) as response:
+        async def provider():
+            async for chunk in response.aiter_bytes(chunk_size=65534):
+                yield chunk
+
+        return Response(200, content=StreamedContent(b"image/jpeg", provider))
 
 
 if __name__ == "__main__":
