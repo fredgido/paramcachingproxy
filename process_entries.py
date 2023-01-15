37cf9cacdb7df323b9ba10989834af85d15a079c
Fred Fernandes <fred.fernandes@neonumy.com>
2023-01-15 23:42:43 +0000

    wip

--- process_entries.py
+++ process_entries.py
@@ -1,4 +1,5 @@
 import asyncio
+import re
 
 import asyncpg
 import orjson
@@ -30,27 +31,64 @@ def extract_tweet_data(entry):
     )
     return tweet
 
+twitter_video_url_regex_str = r"(?P<subdomain>[^.\/]*)\.twimg\.com\/(?P<type>ext_tw_video|amplify_video)/(?P<id>[\d]*)/(?P<pu>pu/)?vid/(?P<resolution>[x\d]*)/(?P<name>[a-zA-Z\d_-]*)(?:\?format=|\.)(?P<extension>[a-zA-Z0-9]{3,4})"
+twitter_url_regex = re.compile(twitter_video_url_regex_str)
 
-def extract_assets_data(entry, tweet_id=None) -> list[dict]:
+
+def twitter_video_url_to_orig(twitter_url: str):
+    match = twitter_url_regex.search(twitter_url)
+    if not match:
+        return None, None, None, None, None
+
+    capture_groups = match.groupdict()
+    return capture_groups["subdomain"], capture_groups["type"], capture_groups["name"], capture_groups["extension"], capture_groups["resolution"]
+
+def extract_assets_data(entry, tweet_id:int) -> list[dict]:
     media = list[dict]()
     if "extended_entities" not in entry:
         return media
     for asset in entry["extended_entities"]["media"]:
-        subdomain, url_type, name, extension = twitter_url_to_orig(asset["media_url_https"])
-        if not name:
-            raise Exception("no name")
-        media_asset = {
-            "id": int(asset["id_str"]),
-            "url": asset["media_url_https"],
-            "width": asset["original_info"]["width"],
-            "height": asset["original_info"]["height"],
-            "post_id": asset.get("source_status_id_str") or tweet_id,
-            "name": name,
-            "extension": extension,
-            "ext_alt_text": asset["ext_alt_text"]
-            # datetime nullable from file request
-        }
-        media.append(media_asset)
+        if asset["type"] == "image":
+            subdomain, url_type, name, extension = twitter_url_to_orig(asset["media_url_https"])
+            if not name:
+                raise Exception("no name")
+            media_asset = {
+                "id": int(asset["id_str"]),
+                "url": asset["media_url_https"],
+                "width": asset["original_info"]["width"],
+                "height": asset["original_info"]["height"],
+                "post_id": int(asset.get("source_status_id_str")) or tweet_id,
+                "name": name,
+                "extension": extension,
+                "ext_alt_text": asset["ext_alt_text"]
+                # datetime nullable from file request
+            }
+            media.append(media_asset)
+        elif asset["type"] == "video":
+            variants = sorted(asset["video_info"]["variants"], key=lambda x: x.get("bitrate", 0), reverse=True)
+            video_url = variants[0]["url"].split("?")[0]
+            subdomain, url_type, name, extension, resolution = twitter_video_url_to_orig(video_url)
+
+            if not name:
+                raise Exception("no name")
+            if not asset.get("source_status_id_str"):
+                post_id_from_url = int(asset["expanded_url"].split("/")[5])
+                if post_id_from_url != tweet_id:
+                    raise Exception("Different id")
+            else:
+                tweet_id = int(asset["source_status_id_str"])
+            media_asset = {
+                "id": int(asset["id_str"]),
+                "url": video_url,
+                "width": asset["original_info"]["width"],
+                "height": asset["original_info"]["height"],
+                "post_id": tweet_id,
+                "name": name,
+                "extension": extension,
+                "ext_alt_text": asset["ext_alt_text"]
+                # datetime nullable from file request
+            }
+            media.append(media_asset)
     return media
 
 
@@ -117,42 +155,45 @@ async def run():
     SELECT id, url, "data", created_at, processed_at
     FROM public.api_dump
     where url like 'https://api.twitter.com/1.1/statuses/home_timeline.json%' and processed_at is null
-    order by id asc limit 10"""
+    order by id asc limit 10000"""
 
-    api_dump_id, url, data, created_at, processed_at = (await db_con.fetch(get_statement))[0]
-    print(url)
-    tweets, assets, users = process_data(orjson.loads(data))
+    for row in (await db_con.fetch(get_statement)):
+        api_dump_id, url, data, created_at, processed_at = row
+        print(url)
+        tweets, assets, users = process_data(orjson.loads(data))
 
-    for user in users.values():
-        user["processed_at"] = created_at
+        for user in users.values():
+            user["processed_at"] = created_at
 
-    values = await db_con.executemany(
-        user_insert_statement, [[user[var] for var in user_vars] for user in users.values()]
-    )
-    print(values)
+        values = await db_con.executemany(
+            user_insert_statement, [[user[var] for var in user_vars] for user in users.values()]
+        )
+        print(values)
 
-    for asset in assets.values():
-        asset["processed_at"] = created_at
-        asset["file_header_date"] = None
+        for asset in assets.values():
+            asset["processed_at"] = created_at
+            asset["file_header_date"] = None
 
-    values = await db_con.executemany(
-        asset_insert_statement, [[asset[var] for var in asset_vars] for asset in assets.values()]
-    )
-    print(values)
+        values = await db_con.executemany(
+            asset_insert_statement, [[asset[var] for var in asset_vars] for asset in assets.values()]
+        )
+        print(values)
 
-    for tweet in tweets.values():
-        tweet["processed_at"] = created_at
-        tweet["file_header_date"] = None
+        for tweet in tweets.values():
+            tweet["processed_at"] = created_at
+            tweet["file_header_date"] = None
 
-    for tweet in tweets.values():
-        try:
-            values = await db_con.execute(post_insert_statement, *[tweet[var] for var in post_vars])
-        except Exception as e:
-            print(values)
-    print(values)
+        values = await db_con.executemany(
+            post_insert_statement, [[tweet[var] for var in post_vars] for tweet in tweets.values()]
+        )
+        print(values)
+
     await db_con.close()
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
     loop.run_until_complete(run())
