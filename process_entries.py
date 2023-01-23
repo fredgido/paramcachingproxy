85e2af2140d0363f055d1083fe0a4c88d5ecac8e
Fred Fernandes <fred.fernandes@neonumy.com>
2023-01-23 02:20:39 +0000

    wip

--- process_entries.py
+++ process_entries.py
@@ -43,11 +43,15 @@ def extract_tweet_data_legacy(entry):
 
 
 def extract_tweet_data_timeline(top_entry):
+    if top_entry.get("__typename") == "TweetWithVisibilityResults" and "tweet" in top_entry:
+        top_entry = top_entry["tweet"]
+
     if top_entry.get("unmention_data"):
-        raise Exception
+        print(top_entry.get("unmention_data"))
     tweet = extract_tweet_data_legacy(top_entry["legacy"])
-    tweet["views"] = int(top_entry["views"].get("count", 0))
-    return tweet
+    tweet["views"] = int(top_entry.get("views", {}).get("count", 0))
+    top_entry.update(tweet)
+    return top_entry
 
 
 twitter_video_url_regex_str = r"(?P<subdomain>[^.\/]*)\.twimg\.com\/(?P<type>ext_tw_video|amplify_video)/(?P<id>[\d]*)/(?P<pu>pu/|pr/)?vid/(?P<resolution>[x\d]*)/(?P<name>[a-zA-Z\d_-]*)(?:\?format=|\.)(?P<extension>[a-zA-Z0-9]{3,4})"
@@ -117,7 +121,7 @@ def extract_assets_data(entry, tweet_id: int) -> list[dict]:
                 "post_id": tweet_id,
                 "name": name,
                 "extension": extension,
-                "ext_alt_text": asset["ext_alt_text"]
+                "ext_alt_text": asset.get("ext_alt_text"),
                 # datetime nullable from file request
             }
             media.append(media_asset)
@@ -137,8 +141,8 @@ def extract_users_data_legacy(entry) -> list[dict]:
             "location": entry["user"]["location"],
             "description": entry["user"]["description"],
             "urls": (
-                [u["expanded_url"] for u in entry["user"]["entities"].get("url", {}).get("urls", [])]
-                + [u["expanded_url"] for u in entry["user"]["entities"]["description"]["urls"]]
+                [u.get("expanded_url", u.get("url")) for u in entry["user"]["entities"].get("url", {}).get("urls", [])]
+                + [u.get("expanded_url", u.get("url")) for u in entry["user"]["entities"]["description"]["urls"]]
             ),
             "protected": entry["user"]["protected"],
             "followers_count": entry["user"]["followers_count"],
@@ -158,58 +162,85 @@ def extract_users_data_legacy(entry) -> list[dict]:
 def extract_users_data_timeline(top_entry):
     legacy = top_entry["legacy"]
     legacy.update(top_entry)
-    tweet = extract_users_data_legacy({"user": legacy})
-    return tweet
+    user = extract_users_data_legacy({"user": legacy})
+    return user
 
 
 def process_data_url(api_data, url: str):
     if url.startswith("https://api.twitter.com/1.1/statuses/home_timeline.json"):
         return process_data_legacy(api_data)
-    return process_timeline_data(api_data)
+    elif url.startswith("https://api.twitter.com/graphql/XMoTnsLCI_a4DyvHNLSoKQ/HomeTimeline"):
+
+        return process_timeline_data(api_data)
+    else:
+        raise Exception
 
 
 def process_timeline_data(data):
-    tweets = dict[int:dict]()
-    assets = dict[tuple[str, str] : dict]()
-    users = dict[int:dict]()
+    tweets_parsed = dict[int:dict]()
+    assets_parsed = dict[tuple[str, str] : dict]()
+    users_parsed = dict[int:dict]()
 
-    data_inside = data["data"]["home"]["home_timeline_urt"]["instructions"]
-    if len(data_inside) > 1:
-        raise Exception
-    data_inside = data_inside[0]["entries"]
+    base_data_inside = data["data"]["home"]["home_timeline_urt"]["instructions"]
+    data_inside = None
+
+    for each in base_data_inside:
+        if each["type"] == "TimelineAddEntries":
+            data_inside = each["entries"]
+        elif each["type"] == "TimelineShowAlert":
+            if each.get("alertType") == "NewTweets":
+                pass
+            else:
+                for user_result in each["usersResults"]:
+                    users_data = extract_users_data_timeline(user_result["result"])
+                    for user_data in users_data:
+                        users_parsed[int(user_data["id"])] = user_data
+
     for tweet_entry in data_inside:
         if not str(tweet_entry["entryId"]).startswith("tweet-"):
             continue
-        print(tweet_entry["content"]["clientEventInfo"]["component"])
-        print(tweet_entry["content"]["itemContent"]["socialContext"])
+        print("clientEventInfo", tweet_entry["content"].get("clientEventInfo",{}).get("component"))
+        print("socialContext", tweet_entry["content"]["itemContent"].get("socialContext"))
         print(tweet_entry["content"]["itemContent"]["tweetDisplayType"])
-        sub_data = tweet_entry["content"]["itemContent"]["tweet_results"]["result"]
+        sub_data = tweet_entry["content"]["itemContent"]["tweet_results"]
+        if not sub_data:
+            continue
+        sub_data=sub_data["result"]
 
         tweet = extract_tweet_data_timeline(sub_data)
-        tweets[int(tweet["id"])] = tweet
-        assets_data = extract_assets_data(sub_data["legacy"], int(tweet["id"]))
+        tweets_parsed[int(tweet["id"])] = tweet
+        assets_data = extract_assets_data(tweet, int(tweet["id"]))
         for asset_data in assets_data:
-            assets[(asset_data["name"], asset_data["extension"])] = asset_data
-        users_data = extract_users_data_timeline(sub_data["core"]["user_results"]["result"])
+            assets_parsed[(asset_data["name"], asset_data["extension"])] = asset_data
+        users_data = extract_users_data_timeline(tweet["core"]["user_results"]["result"])
         for user_data in users_data:
-            users[int(user_data["id"])] = users_data
+            users_parsed[int(user_data["id"])] = user_data
 
-        if sub_data.get("quoted_status_result"):
-            raise Exception
-
-        if sub_data["legacy"].get("retweeted_status_result"):
-            re_tweet = sub_data["legacy"]["retweeted_status_result"]["result"]
-            raise Exception
-            retweet = extract_tweet_data_legacy(tweet_entry["retweeted_status"])
-            tweets[int(retweet["id"])] = retweet
-            retweet_assets_data = extract_assets_data(tweet_entry["retweeted_status"], int(retweet["id"]))
-            for retweet_asset_data in retweet_assets_data:
-                assets[(retweet_asset_data["name"], retweet_asset_data["extension"])] = retweet_asset_data
-            users_data = extract_users_data_legacy(tweet_entry["retweeted_status"])
+        if tweet.get("quoted_status_result"):
+            quote_tweet = tweet["quoted_status_result"]["result"]
+            quotetweet = extract_tweet_data_timeline(quote_tweet)
+            tweets_parsed[int(quotetweet["id"])] = quotetweet
+            assets_data = extract_assets_data(quotetweet.get('legacy'), int(quotetweet["id"]))
+            for asset_data in assets_data:
+                assets_parsed[(asset_data["name"], asset_data["extension"])] = asset_data
+            users_data = extract_users_data_timeline(quotetweet["core"]["user_results"]["result"])
             for user_data in users_data:
-                users[int(user_data["id"])] = users_data
-    return tweets, assets, users
+                users_parsed[int(user_data["id"])] = user_data
 
+        if tweet.get("retweeted_status_result"):
+            raise Exception
+
+        if tweet["legacy"].get("retweeted_status_result"):
+            re_tweet = tweet["legacy"]["retweeted_status_result"]["result"]
+            retweet = extract_tweet_data_timeline(re_tweet)
+            tweets_parsed[int(retweet["id"])] = retweet
+            assets_data = extract_assets_data(retweet.get('legacy'), int(retweet["id"]))
+            for asset_data in assets_data:
+                assets_parsed[(asset_data["name"], asset_data["extension"])] = asset_data
+            users_data = extract_users_data_timeline(tweet["core"]["user_results"]["result"])
+            for user_data in users_data:
+                users_parsed[int(user_data["id"])] = user_data
+    return tweets_parsed, assets_parsed, users_parsed
 
 
 def process_data_legacy(api_data):
@@ -249,7 +280,7 @@ async def run():
     get_statement = """
     SELECT id, url, "data", created_at, processed_at
     FROM public.api_dump
-    where url like 'https://api.twitter.com/1.1/statuses/home_timeline.json%' and processed_at is null
+    where url like 'https://api.twitter.com/graphql/XMoTnsLCI_a4DyvHNLSoKQ/HomeTimeline%' and processed_at is null
     order by id asc limit 10000"""
 
     for i in range(10000):
@@ -265,7 +296,7 @@ async def run():
         for row in rows:
             api_dump_id, url, data, created_at, processed_at = row
             row_ids.append(api_dump_id)
-            if data == b'upstream connect error or disconnect/reset before headers. reset reason: remote reset':
+            if data == b"upstream connect error or disconnect/reset before headers. reset reason: remote reset":
                 continue
             tweets, assets, users = process_data_url(orjson.loads(data), url)
 
